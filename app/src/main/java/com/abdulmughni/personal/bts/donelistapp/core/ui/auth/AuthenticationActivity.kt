8920ed8257dcd5c413f0ba20ab6ab67a050fc4bc
abdulmughnialfikri <dev.abdulmughni@gmail.com>
2021-03-28 00:57:16 +0700

    Fix Back Press

--- app/src/main/java/com/abdulmughni/personal/bts/donelistapp/core/ui/auth/AuthenticationActivity.kt
+++ app/src/main/java/com/abdulmughni/personal/bts/donelistapp/core/ui/auth/AuthenticationActivity.kt
@@ -1,36 +1,23 @@
 package com.abdulmughni.personal.bts.donelistapp.core.ui.auth
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.fragment.app.viewModels
-import com.abdulmughni.personal.bts.donelistapp.R
-import com.abdulmughni.personal.bts.donelistapp.core.domain.model.User
-import com.abdulmughni.personal.bts.donelistapp.core.ui.main.MainActivity
-import com.abdulmughni.personal.bts.donelistapp.core.utils.DataMapper
+import com.abdulmughni.personal.bts.donelistapp.databinding.ActivityAuthenticationBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthenticationActivity : AppCompatActivity() {
 
-    private val viewModel: AuthenticationViewModel by viewModels()
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        viewModel.session.observe(this, {session ->
-            if (session.data != null){
-                viewModel.user.value = DataMapper.convertSessionToUser(session.data)
-                goToMainActivity(DataMapper.convertSessionToUser(session.data))
-            }
-        })
+        supportActionBar?.hide()
+
+
     }
-    private fun goToMainActivity(user: User?) {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra(MainActivity.EXTRA_USER, user)
-        startActivity(intent)
-    }
+
 }
--- app/src/main/java/com/abdulmughni/personal/bts/donelistapp/core/ui/auth/FragmentAuth.kt
+++ app/src/main/java/com/abdulmughni/personal/bts/donelistapp/core/ui/auth/FragmentAuth.kt
@@ -1,15 +1,18 @@
 package com.abdulmughni.personal.bts.donelistapp.core.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.abdulmughni.personal.bts.donelistapp.R
+import com.abdulmughni.personal.bts.donelistapp.core.domain.model.User
+import com.abdulmughni.personal.bts.donelistapp.core.ui.main.MainActivity
+import com.abdulmughni.personal.bts.donelistapp.core.utils.DataMapper
 import com.abdulmughni.personal.bts.donelistapp.databinding.FragmentAuthBinding
-import com.abdulmughni.personal.bts.donelistapp.databinding.FragmentRegisterBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,6 +33,14 @@ class FragmentAuth : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.session.observe(viewLifecycleOwner, {session ->
+            if (session.data != null){
+                viewModel.user.value = DataMapper.convertSessionToUser(session.data)
+                goToMainActivity(DataMapper.convertSessionToUser(session.data))
+            } else {
+                binding.root.visibility = View.VISIBLE
+            }
+        })
         binding.buttonLogin.setOnClickListener {
             findNavController().navigate(R.id.action_to_login)
         }
@@ -38,4 +49,11 @@ class FragmentAuth : Fragment() {
         }
     }
 
+    private fun goToMainActivity(user: User?) {
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.putExtra(MainActivity.EXTRA_USER, user)
+        startActivity(intent)
+        activity?.finish()
+    }
 }
